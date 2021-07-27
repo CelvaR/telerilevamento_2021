a29388d1a2cf3c49a7aee5c689c3675ae8528023
Rob <81300034+CelvaR@users.noreply.github.com>
2021-07-27 10:03:28 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -1,29 +1,23 @@
 library(raster)
 library(rasterVis)
 setwd("C:/Users/Rob/Desktop/UNIBO_05.04.21/Telerilevamento e GIS/lab/greenland")
-rlist <- list.files(pattern='lst') # crea la lista dei file che contengono la stringa 'lst' nel nome
+rlist <- list.files(pattern='lst') # list files containing 'lst' in the name
 # LST = Land Surface Temperature
-inport <- lapply(rlist, raster) # applica "raster" (che inporta singoli files, al contrario di "brick" che inporta oggetti multistrato) agli oggetti in "rlist"
-TGr <- stack(inport) # crea il RasterStack (simile a RasterBrick, ma è piu' un insieme di layers piuttosto che un oggetto multistrato)
+inport <- lapply(rlist, raster) # lapply() applies raster() (which inports single files, countrary to brick() which manages multilayer objects) to objects in 'rlist'
+TGr <- stack(inport) # stack() creates a RasterStack object (similar to a RasterBrick, but is a collection of single-layered objecs insthead of one multilayered one)
 plot(TGr)
 plotRGB(TGr, 1,2,3, stretch='lin')
-# levelplot crea heatmaps. Sull'asse x superiore l'istogramma riporta i vlori medi dei pixel in ogni colonna del raster
-levelplot(TGr$lst_2000)
-# per assegnare colori diversi l'argomento in levelplot è col.regions
+levelplot(TGr$lst_2000) # levelplot() generates a heatmap, with histograms representing mean pixel value by raster row and column
 crp <- colorRampPalette(c('purple', 'blue', 'red'))(200)
-levelplot(TGr$lst_2000, col.regions=crp)
+levelplot(TGr$lst_2000, col.regions=crp) # 'col.region' is levelplot()'s 'col' 
 # plot di tutti gli strati, con titoli e sottotitoli diversi
-levelplot(TGr, col.regions=crp, names.attr=c('2000', '2005', '2010', '2015'), 
-          main = 'Groenlandia')
+levelplot(TGr, col.regions=crp, names.attr=c('2000', '2005', '2010', '2015'), main = 'Groenlandia') # plot all strata, using main title and subtitles
 #
 #
-# immagini del satellite Nimbus7 (lancio 1978). Monta sensore a microonde
-# il ghiaccio non assorbe microonde
-# https://nsidc.org/data/nsidc-0218
-mlist <- list.files(pattern="melt")
+mlist <- list.files(pattern="melt") # Nimbus7 satellite images (launched 1978). Microwave sensor (source: https://nsidc.org/data/nsidc-0218)
 minport <- lapply(mlist, raster)
 melt <- stack(minport)
 levelplot(melt, col.regions=crp)
-# differenza fra i valori di scioglimento fra il primo e l'ultimo anno di volo 
+# difference between current and 1978 melting figures
 melt_amount <- melt$X2007annual_melt - melt$X1979annual_melt
 levelplot(melt_amount)
