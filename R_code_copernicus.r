bca8558f7a503bc8f2575489621c6cf355e76ea8
Rob <81300034+CelvaR@users.noreply.github.com>
2021-07-27 10:10:38 +0200

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -2,11 +2,12 @@ library(raster)
 library(ncdf4)
 setwd("C:/Users/Rob/Desktop/UNIBO_05.04.21/Telerilevamento e GIS/lab")
 #
-# dati del soil water index da VITO observatory. Il file è un singolo strato, quindi si inporta con raster
-
+# VITO observatory Soil Water Index
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html#Browse;Root=514690;Collection=1000281;DoSearch=true;Time=NORMAL,NORMAL,1,JANUARY,2015,31,DECEMBER,2022;isReserved=false
+# single-layered object, inport using raster()
 aqa <- raster('c_gls_SWI1km_202107101200_CEURO_SCATSAR_V1.0.1.nc')
 crp <- colorRampPalette(c('purple', 'blue', 'red'))(200)
 plot(aqa, col=crp)
-# RICAMPIONAMENTO: diminuizione pixel per alleggerire il dato
+# RE-SAMPLING (skim pixel to obtain a lighter object)
 aqa_small <- aggregate(aqa, fact=50)
 plot(aqa_small, col=crp)
